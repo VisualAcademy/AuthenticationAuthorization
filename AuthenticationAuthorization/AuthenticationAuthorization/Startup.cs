16f16c9dda1d60749783fa24c1eaedc180089894
RedPlus <redplus@redplus.net>
2021-02-08 17:14:32 +0900

    Claim, ClaimsIdentity, ClaimsPrincipal 클래스로 최소한의 코드로 인증 쿠키 생성하기

--- AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
+++ AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace AuthenticationAuthorization
 {
@@ -10,6 +13,7 @@ namespace AuthenticationAuthorization
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("Cookies").AddCookie();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -25,7 +29,26 @@ namespace AuthenticationAuthorization
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello .NET!");
+                    string content = "<h1>ASP.NET Core 인증과 권한 초간단 코드</h1>";
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync(content);
+                });
+
+                endpoints.MapGet("/Login", async context => 
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "User Name")
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                    await context.SignInAsync("Cookies", claimsPrincipal);
+
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync("<h3>로그인 완료</h3>");
                 });
             });
         }
