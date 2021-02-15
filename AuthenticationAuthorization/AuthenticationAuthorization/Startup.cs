1cb3c44466b6eb72e589330b0e3dab89779e1991
RedPlus <redplus@redplus.net>
2021-02-16 08:30:58 +0900

    로그인할 때 더 많은 Claim을 저장하고 IsInRole 메서드로 권한을 체크하는 방법 살펴보기

--- AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
+++ AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,36 +33,102 @@ namespace AuthenticationAuthorization
 
             app.UseEndpoints(endpoints =>
             {
+                #region Menu
                 endpoints.MapGet("/", async context =>
                 {
                     string content = "<h1>ASP.NET Core 인증과 권한 초간단 코드</h1>";
 
                     content += "<a href=\"/Login\">로그인</a><br />";
+                    content += "<a href=\"/Login/User\">로그인(User)</a><br />";
+                    content += "<a href=\"/Login/Administrator\">로그인(Administrator)</a><br />";
                     content += "<a href=\"/Info\">정보</a><br />";
+                    content += "<a href=\"/InfoDetails\">정보(Details)</a><br />";
                     content += "<a href=\"/InfoJson\">정보(JSON)</a><br />";
                     content += "<a href=\"/Logout\">로그아웃</a><br />";
 
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
                     await context.Response.WriteAsync(content);
-                });
+                }); 
+                #endregion
 
-                endpoints.MapGet("/Login", async context => 
+                #region /Login/{Username}
+                endpoints.MapGet("/Login/{Username}", async context =>
                 {
+                    var username = context.Request.RouteValues["Username"].ToString(); 
                     var claims = new List<Claim>
                     {
-                        //new Claim(ClaimTypes.Name, "User Name")
-                        new Claim(ClaimTypes.Name, "아이디")
+                        new Claim(ClaimTypes.NameIdentifier, username),
+                        new Claim(ClaimTypes.Name, username),
+                        new Claim(ClaimTypes.Email, username + "@a.com"),
+                        new Claim(ClaimTypes.Role, "Users"),
+                        new Claim("원하는 이름", "원하는 값")
                     };
 
-                    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+                    if (username == "Administrator")
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, "Administrators"));
+                    }
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
                     var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
 
-                    await context.SignInAsync("Cookies", claimsPrincipal);
+                    await context.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        claimsPrincipal,
+                        new AuthenticationProperties { IsPersistent = true });
+
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync("<h3>로그인 완료</h3>");
+                }); 
+                #endregion
+
+                #region Login
+                endpoints.MapGet("/Login", async context =>
+                {
+                    var claims = new List<Claim>
+                    {
+                                //new Claim(ClaimTypes.Name, "User Name")
+                                new Claim(ClaimTypes.Name, "아이디")
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
 
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
                     await context.Response.WriteAsync("<h3>로그인 완료</h3>");
                 });
+                #endregion
+
+                #region InfoDetails
+                endpoints.MapGet("/InfoDetails", async context =>
+                {
+                    string result = "";
+
+                    if (context.User.Identity.IsAuthenticated)
+                    {
+                        result += $"<h3>로그인 이름: {context.User.Identity.Name}</h3>";
+                        foreach (var claim in context.User.Claims)
+                        {
+                            result += $"{claim.Type} = {claim.Value}<br />";
+                        }
+                        if (context.User.IsInRole("Administrators") && context.User.IsInRole("Users"))
+                        {
+                            result += "<br />Administrators + Users 권한이 있습니다.<br />";
+                        }
+                    }
+                    else
+                    {
+                        result += "<h3>로그인하지 않았습니다.</h3>";
+                    }
+
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync(result, Encoding.Default);
+                });
+                #endregion
 
                 #region Info
                 endpoints.MapGet("/Info", async context =>
@@ -92,8 +159,8 @@ namespace AuthenticationAuthorization
                         //json += "{ \"type\": \"Name\", \"value\": \"User Name\" }";
                         var claims = context.User.Claims.Select(c => new ClaimDto { Type = c.Type, Value = c.Value });
                         json += JsonSerializer.Serialize<IEnumerable<ClaimDto>>(
-                            claims, 
-                            new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping }); 
+                            claims,
+                            new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
                     }
                     else
                     {
@@ -106,13 +173,14 @@ namespace AuthenticationAuthorization
                 });
                 #endregion
 
-                endpoints.MapGet("/Logout", async context => 
+                #region 로그아웃
+                endpoints.MapGet("/Logout", async context =>
                 {
-                    await context.SignOutAsync("Cookies");
-                    
+                    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
                     await context.Response.WriteAsync("<h3>로그아웃 완료</h3>");
                 });
+                #endregion
             });
         }
     }
